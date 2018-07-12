c8aa1dedcd9ba8c9116d580aad77066232752140
Rina Lingad <pixie.czar@gmail.com>
2018-07-12 18:33:38 +1200

    Save new spelling words to file

--- SpellingPracticeVer2/Controllers/HomeController.cs
+++ SpellingPracticeVer2/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using SpellingPracticeVer2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -29,6 +31,29 @@ namespace SpellingPracticeVer2.Controllers
 
         public ActionResult UsePrevious()
         {
+            return RedirectToAction("Index", "Setup");
+        }
+
+        [HttpPost]
+        public ActionResult NewWords(SpellingViewModel model)
+        {
+            // Update words file
+            string path = Server.MapPath(@"~/App_Data/words.txt");
+
+            StringBuilder fileContent = new StringBuilder();
+            fileContent.AppendLine(model.Word1)
+                        .AppendLine(model.Word2)
+                        .AppendLine(model.Word3)
+                        .AppendLine(model.Word4)
+                        .AppendLine(model.Word5)
+                        .AppendLine(model.Word6)
+                        .AppendLine(model.Word7)
+                        .AppendLine(model.Word8)
+                        .AppendLine(model.Word9)
+                        .AppendLine(model.Word10);
+
+            System.IO.File.WriteAllText(path, fileContent.ToString());
+
             return RedirectToAction("Index", "Setup");
         }
     }
