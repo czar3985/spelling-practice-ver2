1c8a7bf287e61930c6a2f0c9d3093692e68d9d5d
Rina Lingad <pixie.czar@gmail.com>
2018-07-12 17:32:06 +1200

    Add data annotation [Required] to words and answers

--- SpellingPracticeVer2/Models/SpellingSet.cs
+++ SpellingPracticeVer2/Models/SpellingSet.cs
@@ -15,45 +15,65 @@ namespace SpellingPracticeVer2.Models
     {
         public bool UsePrevious { get; set; }
 
+        [Required]
         [Display(Name = "1.")]
         public string Word1 { get; set; }
+        [Required]
         [Display(Name = "2.")]
         public string Word2 { get; set; }
+        [Required]
         [Display(Name = "3.")]
         public string Word3 { get; set; }
+        [Required]
         [Display(Name = "4.")]
         public string Word4 { get; set; }
+        [Required]
         [Display(Name = "5.")]
         public string Word5 { get; set; }
+        [Required]
         [Display(Name = "6.")]
         public string Word6 { get; set; }
+        [Required]
         [Display(Name = "7.")]
         public string Word7 { get; set; }
+        [Required]
         [Display(Name = "8.")]
         public string Word8 { get; set; }
+        [Required]
         [Display(Name = "9.")]
         public string Word9 { get; set; }
+        [Required]
         [Display(Name = "10.")]
         public string Word10 { get; set; }
 
+        [Required]
         [Display(Name = "1.")]
         public string Answer1 { get; set; }
+        [Required]
         [Display(Name = "2.")]
         public string Answer2 { get; set; }
+        [Required]
         [Display(Name = "3.")]
         public string Answer3 { get; set; }
+        [Required]
         [Display(Name = "4.")]
         public string Answer4 { get; set; }
+        [Required]
         [Display(Name = "5.")]
         public string Answer5 { get; set; }
+        [Required]
         [Display(Name = "6.")]
         public string Answer6 { get; set; }
+        [Required]
         [Display(Name = "7.")]
         public string Answer7 { get; set; }
+        [Required]
         [Display(Name = "8.")]
         public string Answer8 { get; set; }
+        [Required]
         [Display(Name = "9.")]
         public string Answer9 { get; set; }
+        [Required]
         [Display(Name = "10.")]
         public string Answer10 { get; set; }
     }
