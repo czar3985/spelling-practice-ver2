4d3c2d683eb7a600a7b8542c010743704a2c0308
Rina Lingad <pixie.czar@gmail.com>
2018-07-14 18:42:36 +1200

    Save answers in Practice page

--- SpellingPracticeVer2/Controllers/PracticeController.cs
+++ SpellingPracticeVer2/Controllers/PracticeController.cs
@@ -1,7 +1,5 @@
-﻿using System;
+﻿using SpellingPracticeVer2.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace SpellingPracticeVer2.Controllers
@@ -12,6 +10,26 @@ namespace SpellingPracticeVer2.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(SpellingViewModel model)
+        {
+            var list = new List<string> { };
+            list.Add(model.Answer1);
+            list.Add(model.Answer2);
+            list.Add(model.Answer3);
+            list.Add(model.Answer4);
+            list.Add(model.Answer5);
+            list.Add(model.Answer6);
+            list.Add(model.Answer7);
+            list.Add(model.Answer8);
+            list.Add(model.Answer9);
+            list.Add(model.Answer10);
+
+            TempData["answerList"] = list;
+
+            return RedirectToAction("Index", "Result");
         }
     }
 }
