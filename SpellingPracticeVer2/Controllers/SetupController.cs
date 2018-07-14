418e95de87e7e387c0a2adeb954be65d50df2444
Rina Lingad <pixie.czar@gmail.com>
2018-07-14 16:52:43 +1200

    Implement Setup page actions

--- SpellingPracticeVer2/Controllers/SetupController.cs
+++ SpellingPracticeVer2/Controllers/SetupController.cs
@@ -12,11 +12,25 @@ namespace SpellingPracticeVer2.Controllers
         // GET: Setup
         public ActionResult Index()
         {
+            // Get the words from a text file
+            string path = Server.MapPath(@"~/App_Data/words.txt");
+            var fileContent = System.IO.File.ReadAllText(path);
+
             var model = new SpellingSet {
-                words = new List<string> { "exception", "thrown", "system"}
+                words = new List<string> {}
             };
 
+            model.words = fileContent.Split(
+                new[] { Environment.NewLine },
+                StringSplitOptions.None
+            ).ToList();
+
             return View(model);
+        }
+
+        public ActionResult StartTest()
+        {
+            return RedirectToAction("Index", "Practice");
         }
     }
 }
